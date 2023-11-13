1123ad103608930cc3ee74f35fe1bea90c46005c
Justin Cinkelj <justin.cinkelj@xlab.si>
2023-11-13 09:40:30 +0100

    Cleanup - rm .ides, whl/tar files from git
    Signed-off-by: Justin Cinkelj <justin.cinkelj@xlab.si>

--- build/lib/django_live_log/__init__.py
+++
@@ -1,8 +0,0 @@
-from django.conf import settings
-from django.core.exceptions import ImproperlyConfigured
-
-try:
-    settings.DLL_FILE
-except:
-    raise ImproperlyConfigured("Since you are using `django_live_dbug`, set the log file in settings as `DLL_FILE`")
-
--- build/lib/django_live_log/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import re_path
-from django.conf import settings
-from django_live_log.views import dll
-urlpatterns = [
-    re_path(r'^%s$' %('%s/' %getattr(settings, "DLL_URL", "").strip("/") if getattr(settings, "DLL_URL", "") else ""), dll, name="dll"),
-]
